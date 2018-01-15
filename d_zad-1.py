01bd70c785b36b7196799ff12bea122b126a0ce0
pakii <pavlovic.aleksandarr@gmail.com>
2018-01-15 01:21:16 +0100

    second exercise finished

--- d_zad-1.py
+++ d_zad-1.py
@@ -1,51 +1,48 @@
+import random as r
 # -*- coding: utf-8 -*-
 print '\t\tDZ1: Синтакса језика. Типови података. Основне алгоритамске структуре.\n'
-
 
 print '\t1. resenje:\n'
 # 1. Написати програм који на стандардни излаз исписује текст “Здраво, геоинформатичари!” (ћириличним писмом)
 print 'Здраво, геоинформатичари!\n'
 
-
 print '\t2. resenje:\n'
 # 2. Написати програм који за унета два цела броја на стандардни излаз исписује
-    #најпре унете вредности, а затим и њихов збир, разлику, производ, цео део при
-    #дељењу првог броја другим бројем и остатак при дељењу првог броја другим
-    #бројем. Претпоставити да је унос коректан, тј. да друга унета вредност није 0, као
-    #и да се уносе целобројне вредности са стандардног улаза.
+# најпре унете вредности, а затим и њихов збир, разлику, производ, цео део при
+# дељењу првог броја другим бројем и остатак при дељењу првог броја другим
+# бројем. Претпоставити да је унос коректан, тј. да друга унета вредност није 0, као
+# и да се уносе целобројне вредности са стандардног улаза.
 
 num1 = int(raw_input('Unesite ceo broj: '))
 num2 = int(raw_input('Unesite drugi ceo broj: '))
-print 'Prvi uneti broj je {0:d}, a drugi {1:d}.\nNjihov zbir iznosi: {2:d}'.format(num1, num2, num1+num2)
-print 'Razlika: '+str(num1 - num2)
-print 'Proizvod: '+str(num1 * num2)
-print 'Ceo deo pri deljenju prvog sa drugim: '+str(num1 / num2)
-print 'Ostatak pri deljenju prvog sa drugim: '+str(num1 % num2)
-
+print 'Prvi uneti broj je {0:d}, a drugi {1:d}.\nNjihov zbir iznosi: {2:d}'.format(num1, num2, num1 + num2)
+print 'Razlika: ' + str(num1 - num2)
+print 'Proizvod: ' + str(num1 * num2)
+print 'Ceo deo pri deljenju prvog sa drugim: ' + str(num1 / num2)
+print 'Ostatak pri deljenju prvog sa drugim: ' + str(num1 % num2)
 
 print '\n\t3. resenje:\n'
-#3. Написати програм који за унете вредности два правца у облику
-    #степени : минути : секунде, исписује угао који та два правца граде. Угао треба да
-    #буде у децималном запису, заокружен на 4 децимале.
+# 3. Написати програм који за унете вредности два правца у облику
+# степени : минути : секунде, исписује угао који та два правца граде. Угао треба да
+# буде у децималном запису, заокружен на 4 децимале.
 
 print 'Unesite stepene, minute i sekunde DESNOG pravca: '
 RDir = [input('Unesite stepene(0-360): '), input('Unesite minute(0-60): '), input('Unesite sekunde(0-60): ')]
 print 'Unesi stepene, minute i sekunde LEVOG pravca: '
 LDir = [input('Unesite stepene(0-360): '), input('Unesite minute(0-60): '), input('Unesite sekunde(0-60): ')]
 
-decRD = RDir[0] + RDir[1]/60.0 + RDir[2]/3600.0
-decLD = LDir[0] + LDir[1]/60.0 + LDir[2]/3600.0
+decRD = RDir[0] + RDir[1] / 60.0 + RDir[2] / 3600.0
+decLD = LDir[0] + LDir[1] / 60.0 + LDir[2] / 3600.0
 angle = decRD - decLD
 if angle < 0:
     angle = angle + 360
 
 print 'Ugao izmedju unetih pravaca iznosi {0:0.4f} stepeni.'.format(angle)
 
-
 print '\n\t4. resenje:\n'
-#4. Са стандардног улаза се уносе два четвороцифрена броја. Написати програм који
-    #на стандардни излаз исписује суму цифара другог броја, а потом и разлику збира
-    #цифара првог броја на парним и непарним позицијама.
+# 4. Са стандардног улаза се уносе два четвороцифрена броја. Написати програм који
+# на стандардни излаз исписује суму цифара другог броја, а потом и разлику збира
+# цифара првог броја на парним и непарним позицијама.
 
 print 'Unesite dva cetvorocifrena broja: '
 num11 = raw_input('Unesite prvi broj: ')
@@ -58,10 +55,10 @@ sumOdd = 0
 sumEven = 0
 for x in range(0, len(num11)):
     if int(x) % 2 == 0:
-        sumEven += int(num11[x]) #posto iteracija krece od nulte a ne prve pozicije
+        sumEven += int(num11[x])  # posto iteracija krece od nulte a ne prve pozicije
     else:
         sumOdd += int(num11[x])
-print 'Razlika zbira cifara na parnim i neparnim pozicijama prvog unetog broja iznosi %d ' % (sumEven-sumOdd)
+print 'Razlika zbira cifara na parnim i neparnim pozicijama prvog unetog broja iznosi %d ' % (sumEven - sumOdd)
 sumOdd = 0
 sumEven = 0
 for x in num11:
@@ -69,11 +66,10 @@ for x in num11:
         sumOdd += int(x)
     else:
         sumEven += int(x)
-print 'Razlika zbira parnih i neparnih cifara prvog unetog broja iznosi %d \n' % (sumEven-sumOdd)
-
+print 'Razlika zbira parnih i neparnih cifara prvog unetog broja iznosi %d \n' % (sumEven - sumOdd)
 
 print '\n\t5. resenje:\n'
-#5. Написати програм који за унети петоцирени број исписује његову највећу цифру.
+# 5. Написати програм који за унети петоцирени број исписује његову највећу цифру.
 
 fDigits = raw_input('Unesite petocifreni broj: ')
 biggestDigit = -1
@@ -81,46 +77,43 @@ for x in fDigits:
     if int(x) > biggestDigit:
         biggestDigit = int(x)
 
-print 'Najveca cifra unetog broja je %d.'% biggestDigit
-
+print 'Najveca cifra unetog broja je %d.' % biggestDigit
 
 print '\n\t6. resenje:\n'
-#6. Програм учитава пет карактера. Исписати колико пута су се појавиле цифре.
+# 6. Програм учитава пет карактера. Исписати колико пута су се појавиле цифре.
 
 fiveChars = raw_input('Unesite pet karaktera: ')
 count = 0
 for x in fiveChars:
     if x.isdigit():
         count += 1
-print 'Cifra se pojavila %d puta.' %count
-
+print 'Cifra se pojavila %d puta.' % count
 
 print '\n\t7. resenje:\n'
-#7. Написати програм који испитује да ли се дата тачка М(Xm, Ym) налази унутар
-    #троугла чија су темена тачке А(x1, y1), B(x2, y2) и C(x3, y3) и исписује одговор
-    #ДА или НЕ.
+# 7. Написати програм који испитује да ли се дата тачка М(Xm, Ym) налази унутар
+# троугла чија су темена тачке А(x1, y1), B(x2, y2) и C(x3, y3) и исписује одговор
+# ДА или НЕ.
 
-    #METODA BARICENTRICNIH KOORDINATA
+# METODA BARICENTRICNIH KOORDINATA
 
-#sledece vrednosti koordinata su uzete za testiranje resenja
-#nova tacka pada tacno izmedju temena p1 i p2
+# sledece vrednosti koordinata su uzete za testiranje resenja
+# nova tacka pada tacno izmedju temena p1 i p2
 
-#koordinate temena trougla
+# koordinate temena trougla
 
-xp1 = 1.0/10.00
-yp1 = 1.0/9.000
+xp1 = 1.0 / 10.00
+yp1 = 1.0 / 9.000
 
-xp2 = 100.0/8.0
-yp2 = 100.0/3.0
+xp2 = 100.0 / 8.0
+yp2 = 100.0 / 3.0
 
-xp3 = 100.0/4.0
-yp3 = 100.0/9.0
+xp3 = 100.0 / 4.0
+yp3 = 100.0 / 9.0
 
-#koordinate nove tacka
+# koordinate nove tacka
 
-xp = xp1 + (3/7)*(xp2 - xp1)
-yp = yp1 + (3/7)*(yp2 - yp1)
-
+xp = xp1 + (3 / 7) * (xp2 - xp1)
+yp = yp1 + (3 / 7) * (yp2 - yp1)
 
 # koordinate nove tacke se mogu dobiti kao linearna kombinacija koordinata temena:
 #   xp = a * xp1 + b * xp2  + c * xp3
@@ -128,8 +121,8 @@ yp = yp1 + (3/7)*(yp2 - yp1)
 # gde je
 #   a + b + c = 1
 
-a = ((yp2 - yp3)*(xp - xp3) + (xp3 - xp2)*(yp - yp3)) / ((yp2 - yp3)*(xp1 - xp3) + (xp3 - xp2)*(yp1 - yp3))
-b = ((yp3 - yp1)*(xp - xp3) + (xp1 - xp3)*(yp - yp3)) / ((yp2 - yp3)*(xp1 - xp3) + (xp3 - xp2)*(yp1 - yp3))
+a = ((yp2 - yp3) * (xp - xp3) + (xp3 - xp2) * (yp - yp3)) / ((yp2 - yp3) * (xp1 - xp3) + (xp3 - xp2) * (yp1 - yp3))
+b = ((yp3 - yp1) * (xp - xp3) + (xp1 - xp3) * (yp - yp3)) / ((yp2 - yp3) * (xp1 - xp3) + (xp3 - xp2) * (yp1 - yp3))
 c = 1 - a - b
 
 if a <= 1 and b <= 1 and c <= 1 and a >= 0 and b >= 0 and c >= 0:
@@ -137,18 +130,17 @@ if a <= 1 and b <= 1 and c <= 1 and a >= 0 and b >= 0 and c >= 0:
 else:
     print('НЕ')
 
-
 print '\n\t8. resenje:\n'
-#8. Написати програм који имплементира игру ”Погоди број”. На почетку игре
-    #рачунар замишља један случајан број у интервалу [0,100]. Након тога играч уноси
-    #своје име и започиње игру. Играч уноси један по један број све док не погоди који
-    #број је рачунар замислио. Сваки пут када играч унесе број, у зависности од тога
-    #да ли је број који је унет већи или мањи од замишљеног броја исписује се
-    #одговарајућа порука. Игра се завршава у тренутку када играч погодио замишљен
-    #број, када се на стандардном излазу приказује број покушаја из којег је број
-    #погођен
+# 8. Написати програм који имплементира игру ”Погоди број”. На почетку игре
+# рачунар замишља један случајан број у интервалу [0,100]. Након тога играч уноси
+# своје име и започиње игру. Играч уноси један по један број све док не погоди који
+# број је рачунар замислио. Сваки пут када играч унесе број, у зависности од тога
+# да ли је број који је унет већи или мањи од замишљеног броја исписује се
+# одговарајућа порука. Игра се завршава у тренутку када играч погодио замишљен
+# број, када се на стандардном излазу приказује број покушаја из којег је број
+# погођен
 
-import random as r
+
 rand = r.randint(0, 100)
 name = raw_input('Unesite svoje ime: ')
 print 'Zamislio sam broj u intervalu od 0 do 100.\nPogodite koji je to broj...'
--- d_zad-2.py
+++ d_zad-2.py
@@ -1,101 +1,286 @@
 # -*- coding: utf-8 -*-
-print '\t\tDZ2: Фундаменталне структуре података. Библиотека NumPy.\n'
-print '\t1. resenje:\n'
-#1. Написати програм која рачуна суму парних елемената низа. Тестирати га позивом из
-#главног програма.
 import random as r
-niz = [int(100*r.random()) for i in xrange(10)]
-sumEven = 0
-for x in niz:
-	if x % 2 == 0:
-		sumEven += x
-print 'Ulazni niz'
-print niz
-print "Suma elemenata koji su parni brojevi je "+str(sumEven)
 
-sumEvenPos = 0
-for i in range(0, len(niz), 2):
-	sumEvenPos += niz[i]
+print '\t\tDZ2: Фундаменталне структуре података. Библиотека NumPy.\n'
 
-print "Suma elemenata parnim pozicijama "+str(sumEvenPos)
-
-print '\n\t2. resenje:\n'
-#2. Написати програм која рачуна суму елемената низа. Тестирати га позивом из главног
-#програма.
-
-print 'Ulazni niz'
-print niz
-sumNiz = reduce(lambda a, b: a+b, niz)
-print "Suma elemenata niza je "+str(sumNiz)
-
-
-print '\n\t3. resenje:\n'
-#3. Написати програм која рачуна производ елемената низа. Тестирати га позивом из
-#главног програма.
-print 'Ulazni niz'
-print niz
-proNiz = reduce(lambda a, b: a*b, niz)
-print "Proizvod elemenata niza je "+str(proNiz)
-
-
-print '\n\t4. resenje:\n'
-#4. Написати програм која од два низа креира нови низ тако да елементи тог низа
-#представљају наизменична појављивања елемената првог и другог низа, односно
-#другог и првог низа у зависности од унете опције корисника (нпр. за унос опције -п
-#прво се ређају елементи првог па другог, а за унос опције -д прво се ређају елементи
-#другог па првог). Тестирати га позивом из главног програма.
-niz1 = ['*' for i in range(5)]
-niz2 = ['-' for i in range(5)]
-print 'Ovo su dva niza'
-print niz1, niz2
-print 'Sada se kreira novi niz od ova dva...'
-
-userInput = raw_input('Izaberite pocetni clan niza: p ili d')
-if userInput == 'p':
-	rez = niz1
-	for i in range(0, len(niz2), 2):
-		rez.insert(i+1, niz2[i])
-	else:
-		rez.insert(len(rez)-1, niz2[len(niz2)-1])
-	print rez
-else:
-	rez = niz2
-	for i in range(0, len(niz1), 2):
-		rez.insert(i+1, niz1[i])
-	else:
-		rez.insert(len(rez)-1, niz1[len(niz1)-1])
-	print rez
-
-print '\n\t5. resenje:\n'
-#5. Написати програм који за унету реченицу приказује карактере који сe појављују у
-#њој.
-
-
-print '\n\t6. resenje:\n'
-#6. Написати програм који врши фитовање полинома произвољног степена кроз сет
-#унетих тачака (2Д простор). Корисник прво задаје број тачака кроз које провлачи
-#полином, затим уноси X, Y координате тачака. Након што су унете све тачке,
-#корисник дефинише степен полинома. Резултат у виду формуле полинома са
-#одређеним коефицијентима приказује се на стандардном излазу.
-
-import numpy as np
-brTac = input('Zadaj broj tacaka za fitovanje...')
-X = []
-Y = []
-for i in range(1, brTac+1):
-	print 'Tacka %i:' % i
-	X.append(input('unesi X:'))
-	Y.append(input('unesi Y:'))
-stepen = input('Unesi stepen polinoma...')
-fitXY = np.polyfit(X, Y, stepen)
-formula = np.poly1d(fitXY)
-print 'Formula polinoma: %s' % str(formula)
+# print '\n\t1. resenje:\n'
+# #1. Написати програм која рачуна суму парних елемената низа. Тестирати га позивом из
+# #главног програма.
+#
+# niz = [int(100*r.random()) for i in xrange(10)]#niz koji ce sluziti za testiranje
+#
+# sumEven = 0
+# for x in niz:
+# 	if x % 2 == 0:
+# 		sumEven += x
+# print 'Ulazni niz'
+# print niz
+# print "Suma elemenata koji su parni brojevi je "+str(sumEven)
+#
+# sumEvenPos = 0
+# for i in range(0, len(niz), 2):
+# 	sumEvenPos += niz[i]
+#
+# print "Suma elemenata parnim pozicijama "+str(sumEvenPos)
+#
+#
+# print '\n\t2. resenje:\n'
+# #2. Написати програм која рачуна суму елемената низа. Тестирати га позивом из главног
+# #програма.
+#
+# print 'Ulazni niz'
+# print niz
+# sumNiz = reduce(lambda a, b: a+b, niz)
+# print "Suma elemenata niza je "+str(sumNiz)
+#
+#
+# print '\n\t3. resenje:\n'
+# #3. Написати програм која рачуна производ елемената низа. Тестирати га позивом из
+# #главног програма.
+# print 'Ulazni niz'
+# print niz
+# proNiz = reduce(lambda a, b: a*b, niz)
+# print "Proizvod elemenata niza je "+str(proNiz)
+#
+#
+# print '\n\t4. resenje:\n'
+# # 4. Написати програм која од два низа креира нови низ тако да елементи тог низа
+# # представљају наизменична појављивања елемената првог и другог низа, односно
+# # другог и првог низа у зависности од унете опције корисника (нпр. за унос опције -п
+# # прво се ређају елементи првог па другог, а за унос опције -д прво се ређају елементи
+# # другог па првог). Тестирати га позивом из главног програма.
+#
+# niz1 = ['*' for i in range(5)]
+# niz2 = ['-' for i in range(5)]
+# print 'Ovo su dva niza'
+# print niz1, niz2
+# print 'Sada se kreira novi niz od ova dva...'
+#
+# userInput = raw_input('Izaberite pocetni clan niza: p ili d')
+# if userInput == 'п':
+#     rez = niz1
+#     for i in range(0, len(niz2), 2):
+#         rez.insert(i + 1, niz2[i])
+#     else:
+#         rez.insert(len(rez) - 1, niz2[len(niz2) - 1])  # dodaj poslednji clan niza 'niz2' na pretposlednje mesto izlaznog niza
+#     print rez
+# elif userInput == 'д':
+#     rez = niz2
+#     for i in range(0, len(niz1), 2):
+#         rez.insert(i + 1, niz1[i])
+#     else:
+#         rez.insert(len(rez) - 1, niz1[len(niz1) - 1])  # dodaj poslednji clan niza 'niz1' na pretposlednje mesto izlaznog niza
+#     print rez
+#
+#
+# print '\n\t5. resenje:\n'
+# # 5. Написати програм који за унету реченицу приказује карактере који сe појављују у
+# # њој.
+#
+# inputedSentence = raw_input('Unesite zadatu recenicu...').lower()
+# slova = []
+# cifre = []
+# ostalo = []
+# for c in inputedSentence:
+#     if c.isalpha():
+#         slova.append(c)
+#     elif c.isdigit():
+#         cifre.append(c)
+#     else:
+#         ostalo.append(c)
+#
+# slova = set(slova)
+# slova = ','.join(slova)
+# print('Slova koja se pojavljuju: '+slova)
+# cifre = set(cifre)
+# cifre = ','.join(cifre)
+# print('Cifre koje se pojavljuju: '+cifre)
+# ostalo = set(ostalo)
+# ostalo = ''.join(ostalo)
+# print('Ostali karakteri se pojavljuju: '+ostalo)
+#
+#
+# print '\n\t6. resenje:\n'
+# # 6. Написати програм који врши фитовање полинома произвољног степена кроз сет
+# # унетих тачака (2Д простор). Корисник прво задаје број тачака кроз које провлачи
+# # полином, затим уноси X, Y координате тачака. Након што су унете све тачке,
+# # корисник дефинише степен полинома. Резултат у виду формуле полинома са
+# # одређеним коефицијентима приказује се на стандардном излазу.
+#
+# import numpy as np
+#
+# brTac = input('Zadaj broj tacaka za fitovanje...')
+# X = []
+# Y = []
+# for i in range(1, brTac + 1):
+#     print 'Tacka %i:' % i
+#     X.append(input('unesi X:'))
+#     Y.append(input('unesi Y:'))
+# stepen = input('Unesi stepen polinoma...')
+# fitXY = np.polyfit(X, Y, stepen)
+# formula = np.poly1d(fitXY)
+# print 'Formula polinoma: %s' % str(formula)
+#
+#
 print '\n\t7. resenje:\n'
-#7. Написати програм који имплементира игру Ајнц са једним играчем. Игра се са
-#шпилом од 52 карте. На почетку играч уноси своје име након чега рачунар дели две
-#карте играчу и две карте себи. У свакој следећој итерацији рачунар дели по једну
-#карту играчу и себи. Циљ игре је сакупити карте које у збиру имају 21 поен. Карте са
-#бројевима носе онолико бодова колики је број, док жандар, дама, краљ носе 10
-#бодова. Карта Ас може да носи 1 или 10 бодова, у зависности од тога како играчу
-#одговара. Играч који сакупи 21 је победио. Уколико играч премаши 21 бод, победник
-#је његов противник. https://en.wikipedia.org/wiki/Blackjack
+# 7. Написати програм који имплементира игру Ајнц са једним играчем. Игра се са
+# шпилом од 52 карте. На почетку играч уноси своје име након чега рачунар дели две
+# карте играчу и две карте себи. У свакој следећој итерацији рачунар дели по једну
+# карту играчу и себи. Циљ игре је сакупити карте које у збиру имају 21 поен. Карте са
+# бројевима носе онолико бодова колики је број, док жандар, дама, краљ носе 10
+# бодова. Карта Ас може да носи 1 или 10 бодова, у зависности од тога како играчу
+# одговара. Играч који сакупи 21 је победио. Уколико играч премаши 21 бод, победник
+# је његов противник. https://en.wikipedia.org/wiki/Blackjack
+
+# pocetak igre
+igracIme = raw_input('Unesite svoje ime...')
+jednakZbir = False
+nijeKrajIgranja = True
+while nijeKrajIgranja:
+    print('-----------------------------------------------------------------------------')
+    igracKarte = []
+    racunarKarte = []
+    karte = ['as'] + [i for i in range(2, 11)] + [10, 10, 10]
+    karte = karte * 4
+
+    # deljenje karata
+    igracPrvaKarta = r.choice(karte)
+    karte.remove(igracPrvaKarta)
+    igracDrugaKarta = r.choice(karte)
+    karte.remove(igracDrugaKarta)
+
+    if igracPrvaKarta == 'as' and igracDrugaKarta == 'as':
+        igracKarte += [11, 1]
+    elif igracPrvaKarta == 'as' and igracDrugaKarta != 'as':
+        igracKarte += [11, igracDrugaKarta]
+    elif igracPrvaKarta != 'as' and igracDrugaKarta == 'as':
+        igracKarte += [igracPrvaKarta, 11]
+    else:
+        igracKarte += [igracPrvaKarta, igracDrugaKarta]
+
+    print igracIme + ': ', igracKarte
+
+    racunarPrvaKarta = r.choice(karte)
+    karte.remove(racunarPrvaKarta)
+    racunarDrugaKarta = r.choice(karte)
+    karte.remove(racunarDrugaKarta)
+
+    if racunarPrvaKarta == 'as' and racunarDrugaKarta == 'as':
+        racunarKarte += [11, 1]
+    elif racunarPrvaKarta == 'as' and racunarDrugaKarta != 'as':
+        racunarKarte += [11, racunarDrugaKarta]
+    elif racunarPrvaKarta != 'as' and racunarDrugaKarta == 'as':
+        racunarKarte += [racunarPrvaKarta, 11]
+    else:
+        racunarKarte += [racunarPrvaKarta, racunarDrugaKarta]
+
+    print 'racunar: ', [racunarPrvaKarta, '*']
+
+    # ---logika jedne partije---
+
+    # logika racunara
+    zbirRacunara = reduce(lambda a, b: a + b, racunarKarte)
+    # sve dok je zbir karata manji od 16 izvlaci jos jednu kartu
+    while zbirRacunara <= 16:
+
+        # --logika izvlacenja karata--
+        izvucenaKarta = r.choice(karte)
+        karte.remove(izvucenaKarta)
+
+        # ako nije izvucen AS
+        if izvucenaKarta != 'as':
+            if zbirRacunara + izvucenaKarta > 21 and (11 in racunarKarte):
+                racunarKarte.append(izvucenaKarta)
+                racunarKarte.remove(11)
+                racunarKarte.append(1)
+            else:
+                racunarKarte.append(izvucenaKarta)
+
+            zbirRacunara = reduce(lambda a, b: a + b, racunarKarte)
+        else:
+            if (zbirRacunara + 11) > 21:
+                racunarKarte.append(1)
+            else:
+                racunarKarte.append(11)
+
+            zbirRacunara = reduce(lambda a, b: a + b, racunarKarte)
+
+    # logika igraca
+    zbirIgraca = reduce(lambda a, b: a + b, igracKarte)
+    nijeKrajPartije = True
+    while nijeKrajPartije:
+        # ako igrac na pocetku ima 21, momentalno pobedjuje
+        if (igracKarte[0] + igracKarte[1]) == 21:
+            nijeKrajPartije = False
+        else:
+            hitOrStay = raw_input('Za "hit" unesite h; za "stay" unesite s...').lower()
+            if hitOrStay == 'h':
+                # --logika izvlacenja karata--
+                izvucenaKarta = r.choice(karte)
+                karte.remove(izvucenaKarta)
+
+                # ako nije izvucen AS
+                if izvucenaKarta != 'as':
+                    if zbirIgraca + izvucenaKarta > 21 and (11 in igracKarte):
+                        igracKarte.append(izvucenaKarta)
+                        igracKarte.remove(11)
+                        igracKarte.append(1)
+                    else:
+                        igracKarte.append(izvucenaKarta)
+
+                    zbirIgraca = reduce(lambda a, b: a + b, igracKarte)
+                else:
+                    if (zbirIgraca + 11) > 21:
+                        igracKarte.append(1)
+                    else:
+                        igracKarte.append(11)
+
+                    zbirIgraca = reduce(lambda a, b: a + b, igracKarte)
+
+                print igracIme + ': ', igracKarte, zbirIgraca
+
+                # slucaj kada igrac ima jednako ili preko 21
+                if zbirIgraca > 21:
+                    nijeKrajPartije = False
+                    print('IZGUBILI STE...')
+                    print('-----------------------------------------------------------------------------')
+
+                elif zbirIgraca == 21 and zbirRacunara != 21:
+                    nijeKrajPartije = False
+                    print 'racunar: ', racunarKarte, zbirRacunara
+                    print('POBEDILI STE...')
+                    print('-----------------------------------------------------------------------------')
+            # igrac bira stay sto znaci da ima ili tacno ili manje 21
+            elif hitOrStay == 's':
+                nijeKrajPartije = False
+                zbirIgraca = reduce(lambda a, b: a + b, igracKarte)
+                print igracIme + ': ', igracKarte, zbirIgraca
+                print 'racunar: ', racunarKarte, zbirRacunara
+
+                if zbirIgraca == 21 and zbirRacunara != 21:
+                    print('POBEDILI STE...')
+                    print('-----------------------------------------------------------------------------')
+
+                elif zbirIgraca == zbirRacunara and zbirRacunara <= 21:  # igrac i rac su jednaki
+                    jednakZbir = True
+
+                elif zbirIgraca > zbirRacunara and zbirRacunara < 21:  # igrac vise od rac i rac nije presao 21
+                    print('POBEDILI STE...')
+                    print('-----------------------------------------------------------------------------')
+
+                elif zbirIgraca < zbirRacunara <= 21:  # igrac manje i racunar nije presao 21
+                    print('IZGUBILI STE...')
+                    print('-----------------------------------------------------------------------------')
+            # resavanje slucaja kada su zbirovi karata jednaki
+            if jednakZbir:
+                if max(igracKarte) == max(racunarKarte):
+                    print('NERESENO')
+                if max(igracKarte) < max(racunarKarte):
+                    print('IZGUBILI STE...')
+
+    josJednaPartija = raw_input('Za jos jednu partiju unesite "da" ili bilo sta drugo da zavrsite igru...').lower()
+    if josJednaPartija == 'da':
+        nijeKrajIgranja = True
+        nijeKrajPartije = True
+    else:
+        nijeKrajIgranja = False
